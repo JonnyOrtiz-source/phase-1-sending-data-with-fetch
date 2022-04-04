2547db90ace0c019813b2ab98501e8161ab8ff45
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-04-04 15:48:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({ name: name, email: email }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            const body = document.querySelector('body');
+            body.innerHTML = data.id;
+        })
+        .catch(function (error) {
+            const body = document.querySelector('body');
+            body.innerHTML = error;
+        });
+}
+
+submitData('Steve', 'steve@steve.com');
+submitData('Sam', 'sam@sam.com');
